822128591fafc68f18c94829d5e8402c34a2d3c1
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-10-16 16:50:21 +0700

    Menambahkan Composable TataletakColumnRow yang berisi dua Row

--- app/src/main/java/com/example/praktikum2/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2/TataLetak.kt
@@ -59,3 +59,22 @@ fun TataletakBox(modifier: Modifier){
         Text(text = "Column 2")
     }
 }
+
+@Composable
+fun TataletakColumnRow(modifier: Modifier) {
+    Column() {
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Berisi1")
+            Text(text = "Komponen2Berisi1")
+            Text(text = "Komponen3Berisi1")
+        }
+
+        Row (modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Berisi2")
+            Text(text = "Komponen2Berisi2")
+            Text(text = "Komponen3Berisi2")
+        }
+    }
+}
